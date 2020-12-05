81abb05f32a7aa9b21f5098814d3b02cadb6e1b9
Smit <smit.joshi80@gmail.com>
2020-12-05 14:18:02 +0530

    Signed-off-by: Smit <smit.joshi80@gmail.com>

--- 7-questions/app.js
+++ 7-questions/app.js
@@ -1,2 +1,24 @@
 //using selectors inside the element
+const questions = document.querySelectorAll(".question");
+// const btns = document.querySelectorAll(".question-btn");
+questions.forEach(function(ques){
+    const btn = ques.querySelector(".question-btn");
+    btn.addEventListener("click",function(e){
+        questions.forEach(function(item){
+            if(item !== ques){
+                
+                item.classList.remove("show-text");
+            }
+        })
+        
+        ques.classList.toggle("show-text");
+     });
+})
 // traversing the dom
+/*const btns = document.querySelectorAll(".question-btn");
+btns.forEach(function(btn){
+    btn.addEventListener("click",function(e){
+       const question = e.currentTarget.parentElement.parentElement;
+       question.classList.toggle("show-text");
+    });
+});*/
